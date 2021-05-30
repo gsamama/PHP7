8a4485d6af262fc798d48dbb44ccb0c50b2d884e
Gilbert Samama <gsamama@gmail.com>
2021-05-30 20:17:40 +0300

    foreach blablabla...

---
+++ loops/ex03.php
@@ -0,0 +1,8 @@
+<?php
+$meses = array("Jan", "Fev", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+
+foreach($meses as $key => $mes){
+  echo "($key)O mês é:  $mes <br />";
+}
+  
+?>
