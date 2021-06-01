0813a7490b641f6877b5832363cdf56eab86ec47
Gilbert Samama <gsamama@gmail.com>
2021-06-01 17:40:28 +0300

    creating class

---
+++ POO/ex02.php
@@ -0,0 +1,19 @@
+<?php
+
+class Carro{
+  private $modelo;
+  private $motor;
+  private $ano;
+
+  public getModelo(){return $this->modelo;}
+  public setModelo($modelo){$this->modelo = $modelo;}
+
+  public getMotor(){return $this->motor;}
+  public setMotor($motor){$this->motor=$motor;}
+
+  public getAno(){return $this->ano;}
+  public setAno($ano){$this->ano = $ano;}
+
+}
+
+?>
