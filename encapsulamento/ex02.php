4afcdd90e38d885a07f5da84ea298f54cd9b4da6
Gilbert Samama <gsamama@gmail.com>
2021-06-01 19:59:48 +0300

    added encapsulate

---
+++ encapsulamento/ex02.php
@@ -0,0 +1,34 @@
+<?php
+
+  class Pessoa{
+    
+    public $nome = "Rasmus Lerdorf";
+    protected $idade = 48;
+    private $senha = 123456;
+
+    public function verDados(){
+      echo get_class($this) . "<br/>";
+
+      echo $this->nome . "<br />";
+      echo $this->idade . "<br />";
+      echo $this->senha . "<br />";
+    }
+  }
+/*
+  $objeto = new Pessoa();
+  $objeto->verDados();
+*/
+  
+  class Programador extends Pessoa {
+    /*
+    public function verDados(){
+      echo get_class($this) . "<br/>";
+      echo $this->nome . "<br />";
+      echo $this->idade . "<br />";
+      echo $this->senha . "<br />";
+    }
+    */
+  }
+  $objeto2 = new Programador();
+  $objeto2->verDados();
+?>
