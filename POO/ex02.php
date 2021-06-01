facaec3236e65159155e95eed64fafc5f9e3bbfb
Gilbert Samama <gsamama@gmail.com>
2021-06-01 18:07:38 +0300

    removed constructor, not used, using class methods

--- POO/ex02.php
+++ POO/ex02.php
@@ -5,19 +5,27 @@ class Carro{
   private $motor;
   private $ano;
 
-  // constructor
-  public Carro(){}
-
   // getters and setters
-  public getModelo(){return $this->modelo;}
-  public setModelo($modelo){$this->modelo = $modelo;}
+  public function getModelo(){return $this->modelo;}
+  public function setModelo($modelo){$this->modelo = $modelo;}
+  public function getMotor():float{return $this->motor;}
+  public function setMotor($motor){$this->motor=$motor;}
+  public function getAno():int{return $this->ano;}
+  public function setAno($ano){$this->ano = $ano;}
 
-  public getMotor(){return $this->motor;}
-  public setMotor($motor){$this->motor=$motor;}
-
-  public getAno(){return $this->ano;}
-  public setAno($ano){$this->ano = $ano;}
-
+  public function exibir(){
+    return array(
+      'modelo'=> $this->getModelo(), 
+      'motor'=>$this->getMotor(), 
+      'ano'=>$this->getAno()
+    );
+  }
 }
+$gol = new Carro();
+$gol->setModelo("Gol GT");
+$gol->setMotor("1.6");
+$gol->setAno("2017");
+
+var_dump($gol->exibir());
 
 ?>
