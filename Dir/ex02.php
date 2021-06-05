9de17aec9ed794116eb6a3f9f5d16d953d773507
Gilbert Samama <gsamama@gmail.com>
2021-06-05 22:51:38 +0300

    FSO: createdir, removedir, read data in dir

--- Dir/ex02.php
+++ Dir/ex02.php
@@ -1,4 +1,20 @@
 <?php
 $images = scandir("images");
-var_dump($images);
+$data = array();
+foreach ($images as $img) {
+  if(!in_array($img, array(".", ".."))){
+    //echo "<img src='images/$img' />"
+    $filename = "images" . DIRECTORY_SEPARATOR . $img;
+    $info = pathinfo($filename);
+    $info["size"] = filesize($filename);
+    $info["modified"] = date("d/m/Y H:i:s", filemtime($filename));
+
+    $info["url"] = "http://localhost/PHP7/Dir/". str_replace("\\","/", $filename);
+    
+    array_push($data,$info);
+    //var_dump($info);
+  }
+}
+echo json_encode($data);
+//var_dump($images);
 ?>
