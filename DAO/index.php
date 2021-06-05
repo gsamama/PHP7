225f50f3e5d206a65c928a666ec22f2f7c9e728d
Gilbert Samama <gsamama@gmail.com>
2021-06-05 19:18:11 +0300

    Usuario class created

--- DAO/index.php
+++ DAO/index.php
@@ -4,9 +4,13 @@ require_once("config.php");
 
 $sql = new Sql();
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);
+$usuario = new Usuario();
 
+$usuario->loadById(2);
+
+//echo json_encode($usuarios);
+echo $usuario;
 
 ?>
